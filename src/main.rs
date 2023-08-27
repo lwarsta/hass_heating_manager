bef1f078c08f4b14f43cecd05c398e0dab8a9ab4
lassi@warsta.net <lassi@warsta.net>
2023-08-27 10:16:02 +0300

    Create a simple server solution as base for the application.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-// main.rs
-fn main() {
-  println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // Define a filter that responds to GET requests at the "/hello" path
+    let hello = warp::path!("hello" / "world")
+        .map(|| warp::reply::html("Hello, world!"));
+
+    // Combine the filters and start the warp server
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
