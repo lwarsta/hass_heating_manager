1dab1cc4136e6048fb40082b577de297950de6fd
lassi@warsta.net <lassi@warsta.net>
2023-08-27 11:28:16 +0300

    Post command tested.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use warp::Filter;
+use warp::{Filter, Reply};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct MyData {
+    field1: String,
+    field2: i32,
+}
 
 #[tokio::main]
 async fn main() {
-    // Define a filter that responds to GET requests at the "/hello" path
-    let hello = warp::path!("hello" / "world")
-        .map(|| warp::reply::html("Hello, world!"));
+    // Define a filter for the specific path and POST method
+    let my_route = warp::path!("my_route" / "post")
+        .and(warp::post())
+        .and(warp::body::json()) // Automatically deserialize JSON data
+        .map(|data: MyData| {
+            // Handle the received data
+            warp::reply::json(&data) // Echo the received data as JSON
+        });
 
-    // Combine the filters and start the warp server
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    // Combine filters and start the warp server
+    warp::serve(my_route).run(([127, 0, 0, 1], 3030)).await;
 }
