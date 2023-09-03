faed9dbf0f69924522b49a4b123334100a6cbb55
lwlassi <lwlassi@ESPKT3K2197>
2023-09-03 11:15:20 +0300

    Build profiles were added.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use warp::{Filter, Reply};
 use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use serde_yaml;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct MyData {
@@ -7,8 +9,35 @@ struct MyData {
     field2: i32,
 }
 
+#[derive(Deserialize, Debug)]
+struct Config {
+    ip_address: String,
+    port: String,
+    token: String,
+}
+
 #[tokio::main]
 async fn main() {
+    // Define the path to the configuration file
+    let config_path = "local_data/config_data.yaml";
+	
+    // Read the configuration from the YAML file
+    let config_str = std::fs::read_to_string(config_path)
+        .expect("Failed to read config_tmp.yaml");
+    let config: Config = serde_yaml::from_str(&config_str)
+        .expect("Failed to parse YAML");
+	
+    // Parse the IP address and port from the config
+    let ip_address_str = &config.ip_address;
+    let port_str = &config.port;
+	let token_str = &config.token;
+
+    // Combine IP address and port into a single string
+    let ip_port_str = format!("{}:{}", ip_address_str, port_str);
+
+    // Parse the combined string into a SocketAddr
+    let ip_address: SocketAddr = ip_port_str.parse().unwrap();
+	
     // Define a filter for the specific path and POST method
     let my_route = warp::path!("my_route" / "post")
         .and(warp::post())
@@ -20,6 +49,9 @@ async fn main() {
             warp::reply::json(&data) // Echo the received data as JSON
         });
 
+    // Print a message indicating that the server is starting
+    println!("Server started at {}", ip_address);
+
     // Combine filters and start the warp server
-    warp::serve(my_route).run(([127, 0, 0, 1], 3030)).await;
+	warp::serve(my_route).run(ip_address).await;
 }
